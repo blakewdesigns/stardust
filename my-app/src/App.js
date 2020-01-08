e0a2d6148c2bf0c794883e059531a12813856dd2
Blake Walker <blake.wdesigns@gmail.com>
2020-01-08 14:53:51 -0500

    Finished nav and started hero

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,22 +4,45 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <body>
+      <header>
+        <div className="nav__wrapper">
+          <div className="nav__left">
+            <div className="nav__logo_wrapper">
+              <img className="nav__logo" src={require("./images/KALogoMain.png")} alt="logo"></img>
+            </div>
+          </div>
+          <div className="nav__right">
+            <a className="nav__link" href="#">Programs</a>
+            <a className="nav__link" href="#">About</a>
+            <a className="nav__link" href="#">Admissions</a>
+            <a className="nav__link" href="#">Connect</a>
+            <a className="nav__link" href="#">Blog</a>
+            <button className="nav__btn" href="#">Apply Now</button>
+          </div>
+        </div>
       </header>
-    </div>
+      <div className="hero">
+        <p className="hero__heading">Welcome to Kenzie Academy</p>
+        <img className="hero__img" src={require("./images/KenzieHero.png")} alt="hero image"></img>
+      </div>
+    </body>
   );
 }
 
