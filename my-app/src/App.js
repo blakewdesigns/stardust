11721d2f6772387882a4f19774f3454fd81e2b5e
Blake Walker <blake.wdesigns@gmail.com>
2020-01-09 11:45:26 -0500

    Updated Hero - WIP

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,22 +4,6 @@ import './App.css';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <body>
       <header>
         <div className="nav__wrapper">
@@ -40,7 +24,7 @@ function App() {
       </header>
       <div className="hero">
         <p className="hero__heading">Welcome to Kenzie Academy</p>
-        <img className="hero__img" src={require("./images/KenzieHero.png")} alt="hero image"></img>
+        {/* <img className="hero__img" src={require("./images/KenzieHero.png")} alt="hero image"></img> */}
       </div>
     </body>
   );
